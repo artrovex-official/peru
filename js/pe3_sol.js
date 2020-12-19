c4dd2ee9845201dee0fb9998183a85c0e7d9e828
artrovex-official <76234442+artrovex-official@users.noreply.github.com>
2020-12-19 17:44:46 +0200

    2

---
+++ js/pe3_sol.js
@@ -0,0 +1,18 @@
+﻿(function() {
+    "use strict";
+    window.countryList = {
+          pe: {
+            name: 'Perú',
+            code: 'pe',
+            price: "137.00",
+            oldPrice: "274.00",
+            labelPrice: "S/.",
+            phoneHelper: 'Por ejemplo, +51 123456789',
+            nameHelper: 'Alejandro Rodriguez',
+            nameError: 'Nombre (campo obligatorio)',
+            phoneError: 'Número incorrectamente especificado',
+            countryError: 'Debe introducir un país',
+            isDefault: true       
+        }
+    };
+})();
